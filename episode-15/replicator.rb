485fdc27408abfd9527a48e08b1f7dec6db23e5c
lor104 <lauren.rosentzveig@gmail.com>
2017-04-03 17:11:03 -0400

    finish episode 15

--- episode-15/replicator.rb
+++ episode-15/replicator.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Replicator
 
   attr_reader :plate
@@ -24,7 +25,7 @@ class Replicator
   end
 
   def retrieve_glass
-    @enterprise.transporter.energize(obj: @enterprise.cupboard.find_glass, from: @enterprise.cupboard.shelf, to: @enterprise.cupboard.shelf)
+    @enterprise.transporter.energize(obj: @enterprise.cupboard.find_glass, from: @enterprise.cupboard.shelf, to: @tummy)
   end
 
   def glass_in_tummy
